646b26132b8d3f9d1b04de15e03d095510a7322f
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-17 14:38:40 +0530

    User password hashed

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   method: {
@@ -24,6 +25,12 @@ const UserSchema = new mongoose.Schema({
       lowercase: true,
     },
   },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (this.method !== 'local') next();
+  const salt = await bcrypt.genSalt(10);
+  this.local.password = await bcrypt.hash(this.local.password, salt);
 });
 
 const User = mongoose.model('User', UserSchema);
